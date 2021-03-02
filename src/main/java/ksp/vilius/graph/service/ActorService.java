c617b5c31e540e74723477c3fc89b59d87fab025
vilksp <v.kasperavicius1@gmail.com>
2021-03-02 20:47:42 +0200

    getAll method

--- src/main/java/ksp/vilius/graph/service/ActorService.java
+++ src/main/java/ksp/vilius/graph/service/ActorService.java
@@ -1,19 +1,26 @@
 package ksp.vilius.graph.service;
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import ksp.vilius.graph.model.Actor;
 import ksp.vilius.graph.repositories.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ActorService {
+public class ActorService implements GraphQLQueryResolver {
 
     @Autowired
     private ActorRepository repository;
 
     public List<Actor> getAllActors() {
         return repository.findAll();
+    }
+
+    public Optional<Actor> getActorById(Long id) {
+
+        return repository.findById(id);
     }
 }
--- src/main/java/ksp/vilius/graph/service/FilmService.java
+++ src/main/java/ksp/vilius/graph/service/FilmService.java
@@ -1,2 +1,21 @@
-package ksp.vilius.graph.service;public class FilmService {
+package ksp.vilius.graph.service;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import ksp.vilius.graph.model.Film;
+import ksp.vilius.graph.repositories.FilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FilmService implements GraphQLQueryResolver {
+
+    @Autowired
+    private FilmRepository repository;
+
+    public List<Film> getAllFilms() {
+
+        return repository.findAll();
+    }
 }
