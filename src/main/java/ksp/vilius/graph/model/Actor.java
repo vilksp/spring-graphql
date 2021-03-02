9303d17d841faccb25d4214ada79efb1cc9e32b1
vilksp <v.kasperavicius1@gmail.com>
2021-03-02 19:23:42 +0200

    actor and film entities

--- src/main/java/ksp/vilius/graph/model/Actor.java
+++ src/main/java/ksp/vilius/graph/model/Actor.java
@@ -1,4 +1,22 @@
 package ksp.vilius.graph.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
 }
--- src/main/java/ksp/vilius/graph/model/Film.java
+++ src/main/java/ksp/vilius/graph/model/Film.java
@@ -1,2 +1,20 @@
-package ksp.vilius.graph.model;public class Film {
+package ksp.vilius.graph.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
