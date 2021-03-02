6f1208e0ff0b39092d706fffa771c4704f25885f
vilksp <v.kasperavicius1@gmail.com>
2021-03-02 15:14:50 +0200

    initial commit

---
+++ src/main/java/ksp/vilius/graph/SpringGraphApplication.java
@@ -0,0 +1,13 @@
+package ksp.vilius.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksp/vilius/graph/SpringGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package ksp.vilius.graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
