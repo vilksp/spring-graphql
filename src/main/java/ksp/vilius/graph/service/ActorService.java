bdc3e0752a85556a9aea22d26da2e07748d2b965
vilksp <v.kasperavicius1@gmail.com>
2021-03-02 19:40:47 +0200

    getAllActors method added

--- src/main/java/ksp/vilius/graph/service/ActorService.java
+++ src/main/java/ksp/vilius/graph/service/ActorService.java
@@ -1,2 +1,19 @@
-package ksp.vilius.graph.service;public class ActorService {
+package ksp.vilius.graph.service;
+
+import ksp.vilius.graph.model.Actor;
+import ksp.vilius.graph.repositories.ActorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ActorService {
+
+    @Autowired
+    private ActorRepository repository;
+
+    public List<Actor> getAllActors() {
+        return repository.findAll();
+    }
 }
