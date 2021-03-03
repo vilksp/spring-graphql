a0bd20a7aa62b91f4109e975a269e73d09a843b9
vilksp <v.kasperavicius1@gmail.com>
2021-03-03 19:43:08 +0200

    graphql mutation

--- src/main/java/ksp/vilius/graph/service/ActorService.java
+++ src/main/java/ksp/vilius/graph/service/ActorService.java
@@ -1,16 +1,18 @@
 package ksp.vilius.graph.service;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import ksp.vilius.graph.model.Actor;
 import ksp.vilius.graph.repositories.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ActorService implements GraphQLQueryResolver {
+public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private ActorRepository repository;
@@ -22,5 +24,12 @@ public class ActorService implements GraphQLQueryResolver {
     public Optional<Actor> getActorById(Long id) {
 
         return repository.findById(id);
+    }
+
+    @Transactional
+    public Actor updateAddress(Long id, String address) {
+        Actor actor = repository.findById(id).get();
+        actor.setAddress(address);
+        return repository.save(actor);
     }
 }
